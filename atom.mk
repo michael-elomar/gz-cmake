b2888f79ac610b1512ffe3d96dc4ecf9720805b1
Michael El Omar <michaelelomar@outlook.com>
2026-04-05 22:47:04 +0200

    add atom.mk

---
+++ atom.mk
@@ -0,0 +1,22 @@
+
+LOCAL_PATH := $(call my-dir)
+
+################################################################################
+# gz-cmake
+################################################################################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := gz-cmake
+LOCAL_DESCRIPTION := A set of CMake modules that are used by the C++-based Gazebo projects.
+
+LOCAL_LIBRARIES := \
+	assimp
+
+LOCAL_CMAKE_CONFIGURE_ARGS := \
+	-DBUILD_TESTING:BOOL=False
+
+LOCAL_EXPORT_C_INCLUDES := $(TARGET_OUT_STAGING)/usr/include/gz/cmake5
+
+include $(BUILD_CMAKE)
+
